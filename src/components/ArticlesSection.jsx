0ef67a2f24eee7246b1ff035b2063677a07c980a
vicente jorquera <vjorquera@tibox.cl>
2023-01-29 19:44:01 -0300

    home layout for medium & large screens fully working

--- src/components/ArticlesSection.jsx
+++ src/components/ArticlesSection.jsx
@@ -17,12 +17,17 @@ export const ArticleSection = () => {
     setVisible(!visible)
   }
 
+  const cleanFilter = () => {
+    if (visible) setVisible(!visible)
+    setFilter('')
+  }
+
   return (
     <section className='w-full py-4 dark:text-stone-100 my-2 flex flex-col gap-4'>
       <header className='flex gap-4 justify-between'>
         <h2 className='text-2xl font-semibold'>Artículos recientes</h2>
         <div className='flex gap-4 items-center text-xl'>
-          <MdCleaningServices onClick={() => setFilter('')} className='hover:fill-teal-500 cursor-pointer transition-all duration-500' />
+          <MdCleaningServices onClick={cleanFilter} className='hover:fill-teal-500 cursor-pointer transition-all duration-500' />
           <div className='relative border-2 rounded-lg text-sm border-zinc-600 p-2 cursor-pointer select-none' onClick={handleVisible}>
             <p>Ordernar por</p>
             <div className={`${visible ? 'block' : 'hidden'} asbolute p-2 border-2 border-zinc-600 absolute top-9 left-0 bg-white dark:bg-zinc-900 rounded-lg select-none text-sm cursor-pointer leading-6`}>
--- src/components/CarouselSlide.jsx
+++ src/components/CarouselSlide.jsx
@@ -5,8 +5,8 @@ export const CarouselSlide = ({ id, title, subtitle, linkToArticle, imageURL }) 
     <div className="bg-gray-500 bg-cover inset-0 bg-center bg-no-repeat h-auto md:h-80 2xl:h-96 rounded-lg  " style={{ backgroundImage: `url("${imageURL}")` }}>
       <Link href={linkToArticle} className=' border border-transparent transition-all duration-500 hover:border-teal-500 absolute cursor-pointer inset-0 bg-black bg-opacity-75 rounded-lg'>
         <div className='content absolute w-full bottom-8 px-8 flex flex-col gap-4'>
-          <h2 className='text-xl md:text-3xl font-bold'>{title}</h2>
-          <p className='font-light text-lg'>{subtitle}</p>
+          <h2 className='text-xl md:text-2xl xl:text-3xl font-bold'>{title}</h2>
+          <p className='font-light text-sm xl:text-lg'>{subtitle}</p>
         </div>
       </Link>
     </div>
